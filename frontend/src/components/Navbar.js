af40a96bc74925bd465e36a7ba9431a10f1c6874
danifeerrer <daniel.ferrer80@myhunter.cuny.edu>
2023-12-08 13:40:19 -0500

    Navbar changed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-<<<<<<< HEAD
-import { AppBar, Toolbar, Button } from '@mui/material';
-import logo from '../components/images/logo.png';
-=======
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link, NavLink as Router } from 'react-router-dom'; // Import Link
 import logo from '../data/logo.png';
->>>>>>> branch-1
 
 const Navbar = () => {
   const navBarStyle = {
     position: 'fixed',
-<<<<<<< HEAD
-    left: 0,
-    right: 0,
-    top: 0,
-    background: '#2c3e50',
-=======
     background: '#2c3e50'
   };
 
   const navLink = {
     textDecoration: 'none',
->>>>>>> branch-1
   };
 
   const buttonStyle = {
@@ -34,8 +22,7 @@ const Navbar = () => {
     fontSize: '18px',
     fontWeight: 'bold',
     color: '#ecf0f1',
-    marginLeft: '30px', // Adjust the left margin
-    marginRight: '50px', // Adjust the right margin
+    marginLeft: '10px',
     ":hover": {
       backgroundColor: '#3498db',
       color: '#ecf0f1',
@@ -55,16 +42,12 @@ const Navbar = () => {
     <AppBar sx={navBarStyle}>
       <Toolbar>
         <img src={logo} width="100px" alt="Logo" />
-<<<<<<< HEAD
-        <NavLink to="/" activeClassName="active" className="nav-link">
-=======
         <NavLink
           to="/"
           activeClassName="active"
           className="nav-link"
           style={navLink}
         >
->>>>>>> branch-1
           <Button sx={buttonStyle} color="inherit">
             Home
           </Button>
@@ -72,13 +55,10 @@ const Navbar = () => {
 
         <NavLink to="/code" activeClassName="active" className="nav-link" style={navLink}>
           <Button sx={buttonStyle} color="inherit">
-            CNN Code
+            Code
           </Button>
         </NavLink>
 
-<<<<<<< HEAD
-        <NavLink to="/MRI" activeClassName="active" className="nav-link">
-=======
         {/* Conditionally render the "Login" link */}
         {!isLoggedIn && (
           <NavLink to="/login" activeClassName="active" className="nav-link" style={navLink}>
@@ -94,21 +74,16 @@ const Navbar = () => {
           className="nav-link"
           style={navLink}
         >
->>>>>>> branch-1
           <Button sx={buttonStyle} color="inherit">
             MRI
           </Button>
         </NavLink>
-<<<<<<< HEAD
-        <NavLink to="/StrokeCal" activeClassName="active" className="nav-link">
-=======
         <NavLink
           to="/StrokeCal"
           activeClassName="active"
           className="nav-link"
           style={navLink}
         >
->>>>>>> branch-1
           <Button sx={buttonStyle} color="inherit">
             Calculate Stroke
           </Button>
